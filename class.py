cd32b34b1666d5fa1e07bc4ebbbad4f6ea6bb7a9
GovorunV <govorun.vlad@gmail.com>
2019-04-08 13:12:58 +0300

    add polimorfizm

--- class.py
+++ class.py
@@ -23,6 +23,7 @@ print(bob.name)
 bob.printall()
 '''
 #Использование конструктора
+'''
 class User:
     name=""
     srname=""
@@ -46,3 +47,22 @@ admin.printall()
 
 bob=User("Bob","Kolaw","23","ssds@mail.ru")
 bob.printall()
+'''
+class User:
+    _name=""
+    srname=""
+    age=""
+    email=""
+
+    def set(self,name,srname,age): #self обезательный
+        self._name=name #инкапсуляция _ ( не обезательно) или __
+        self.srname=srname
+        self.age=age
+    def printall(self):
+        print("Пользователь",self.name,"его возраст",self.age)
+class Member (User): #наследния класа(полимирфизм) можно использовать все методы и переменые функции Юзер
+    def printall(self): #переназначение метода
+        print("Пользователь",self._name,"Фамилия",self.srname,"его возраст",self.age)
+x3=Member()
+x3.set("sfsdf","dfsd","23")
+x3.printall()
